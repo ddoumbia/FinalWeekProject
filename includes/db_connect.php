443df2810c650b03b7462129ddc3c057b292e6ef
Zane DeVore <66222562+annopolis230@users.noreply.github.com>
2024-12-12 16:21:04 -0800

    remove die statement

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -9,8 +9,4 @@ $dbname = "db";     // Database name
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
 ?>
