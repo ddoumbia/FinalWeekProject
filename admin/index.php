a7010dcb498c73e60433aecd12324107750cb674
Zane DeVore <66222562+annopolis230@users.noreply.github.com>
2024-12-12 16:33:21 -0800

    fixed file structure for real this time

--- admin/index.php
+++ admin/index.php
@@ -1,3 +1,4 @@
+<base href="/FinalWeekProject/">
 <?php
 include('../includes/header.php');
 include('../includes/auth.php');
@@ -5,10 +6,10 @@ check_admin();
 ?>
 <h2>Admin Dashboard</h2>
 <ul class="list-group mt-3">
-    <li class="list-group-item"><a href="manage_users.php">Manage Users</a></li>
-    <li class="list-group-item"><a href="../pets/index.php">Manage Pets</a></li>
-    <li class="list-group-item"><a href="../posts/index.php">Manage Posts</a></li>
-    <li class="list-group-item"><a href="../pages/index.php">Manage Pages</a></li>
+    <li class="list-group-item"><a href="./admin/manage_users.php">Manage Users</a></li>
+    <li class="list-group-item"><a href="./pets/index.php">Manage Pets</a></li>
+    <li class="list-group-item"><a href="./posts/index.php">Manage Posts</a></li>
+    <li class="list-group-item"><a href="./pages/index.php">Manage Pages</a></li>
 </ul>
 <?php
 include('../includes/footer.php');
