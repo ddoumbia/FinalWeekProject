e437e0d7f8dcf430b2c03cd8e1b4ca3ed846d005
Zane DeVore <66222562+annopolis230@users.noreply.github.com>
2024-12-12 16:22:54 -0800

    fixed file structure for real this time

--- posts/index.php
+++ posts/index.php
@@ -5,13 +5,14 @@ include('../includes/header.php');
 $sql = "SELECT p.*, u.username FROM Post p JOIN User u ON p.author_id = u.user_id ORDER BY date_created DESC";
 $result = $conn->query($sql);
 ?>
+<base href="/FinalWeekProject/">
 <h2>All Posts</h2>
 <?php if(isset($_SESSION['user_id'])): ?>
     <a href="create.php" class="btn btn-primary">Create New Post</a>
 <?php endif; ?>
 <div class="list-group mt-3">
     <?php while ($post = $result->fetch_assoc()): ?>
-        <a href="view.php?id=<?php echo $post['post_id']; ?>" class="list-group-item list-group-item-action">
+        <a href="./posts/view.php?id=<?php echo $post['post_id']; ?>" class="list-group-item list-group-item-action">
             <h5 class="mb-1"><?php echo htmlspecialchars($post['title']); ?></h5>
             <small>By <?php echo htmlspecialchars($post['username']); ?> on <?php echo htmlspecialchars($post['date_created']); ?></small>
         </a>
