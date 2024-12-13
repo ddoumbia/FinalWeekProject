4e5eeb95136ecbc89b80c30189f21e4c51fa89df
Zane DeVore <66222562+annopolis230@users.noreply.github.com>
2024-12-12 16:30:51 -0800

    fixed file structure for real this time

--- pages/index.php
+++ pages/index.php
@@ -1,3 +1,4 @@
+<base href="/FinalWeekProject/">
 <?php
 include('../includes/header.php');
 
@@ -8,14 +9,14 @@ $result = $stmt->get_result();
 ?>
 <h2>Pages</h2>
 <?php if(isset($_SESSION['is_admin']) && $_SESSION['is_admin']): ?>
-    <a href="edit.php" class="btn btn-primary">Create New Page</a>
+    <a href="./pages/edit.php" class="btn btn-primary">Create New Page</a>
 <?php endif; ?>
 <ul class="list-group mt-3">
     <?php while ($page = $result->fetch_assoc()): ?>
         <li class="list-group-item">
-            <a href="view.php?slug=<?php echo urlencode($page['slug']); ?>"><?php echo htmlspecialchars($page['title']); ?></a>
+            <a href="./pages/view.php?slug=<?php echo urlencode($page['slug']); ?>"><?php echo htmlspecialchars($page['title']); ?></a>
             <?php if(isset($_SESSION['is_admin']) && $_SESSION['is_admin']): ?>
-                <a href="edit.php?id=<?php echo $page['page_id']; ?>" class="btn btn-sm btn-secondary float-right">Edit</a>
+                <a href="./pages/edit.php?id=<?php echo $page['page_id']; ?>" class="btn btn-sm btn-secondary float-right">Edit</a>
             <?php endif; ?>
         </li>
     <?php endwhile; ?>
